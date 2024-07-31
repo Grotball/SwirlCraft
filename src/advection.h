97431698d6cab20223c32c4dab443394179e4102
Grotball <140941449+Grotball@users.noreply.github.com>
2024-07-31 11:19:45 +0800

    Add bilinear interpolation

--- src/advection.h
+++ src/advection.h
@@ -24,6 +24,35 @@ namespace SwirlCraft
             const auto index = linearIndex(I, domain);
             return f[index];
         }
+        
+        template <typename T>
+        T domainBilinearInterpolate(T* f, const T(&x)[2UL], const Domain<T, 2UL>& domain)
+        {
+            int32_t ci = static_cast<int32_t>(std::round(x[0]));
+            int32_t cj = static_cast<int32_t>(std::round(x[1]));
+
+            int32_t i1 = static_cast<int32_t>(std::floor(x[0]));
+            int32_t i2 = static_cast<int32_t>(std::ceil(x[0]));
+            int32_t j1 = static_cast<int32_t>(std::floor(x[1]));
+            int32_t j2 = static_cast<int32_t>(std::ceil(x[1]));
+            auto index11 = i1 * domain.dims[0].stride + j1 * domain.dims[1].stride;
+            auto index12 = i1 * domain.dims[0].stride + j2 * domain.dims[1].stride;
+            auto index21 = i2 * domain.dims[0].stride + j1 * domain.dims[1].stride;
+            auto index22 = i2 * domain.dims[0].stride + j2 * domain.dims[1].stride;
+
+            auto a11 = f[index11];
+            auto a12 = f[index12];
+            auto a21 = f[index21];
+            auto a22 = f[index22];
+
+            T s = x[0] - i1;
+            T t = x[1] - j1;
+
+            T b1 = lerp(a11, a12, t);
+            T b2 = lerp(a21, a22, t);
+            
+            return lerp(b1, b2, s);
+        }
     }
     
     template <typename T, size_t Dims>
