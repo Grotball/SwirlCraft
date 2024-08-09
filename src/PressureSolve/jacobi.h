d772c6cd962fffe66afdf83de5453ef261f36669
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-09 11:59:51 +0800

    Refactor

--- src/PressureSolve/jacobi.h
+++ src/PressureSolve/jacobi.h
@@ -4,6 +4,31 @@
 
 namespace SwirlCraft
 {
+    template <typename T>
+    void jacobiIteration(T* p, T* p_old, const T* div, const T* collision, const T c0, const T* c, const int64_t* strides, const uint32_t dims, const size_t N)
+    {
+        for (size_t i = 0; i < N; i++)
+        {
+            p_old[i] = p[i];
+        }
+
+        for (size_t i = 0; i < N; i++)
+        {
+            if (collision[i] > 0)
+            {
+                T A = 0;
+                for (size_t j = 0; j < dims; j++)
+                {
+                    const auto stride = strides[j];
+                    const T b1 = collision[i-stride] > 0 ? p_old[i-stride] : p_old[i];
+                    const T b2 = collision[i+stride] > 0 ? p_old[i+stride] : p_old[i]; 
+                    A += c[j] * (b1 + b2);
+                }
+                p[i] = A - c0 * div[i];
+            }
+        }
+    }
+
     template <typename T, uint32_t Dims>
     void jacobiSolve(T* p, const T* div, const T* collision, const Grid<T, Dims>& grid, const int32_t maxIterations)
     {
@@ -30,27 +55,7 @@ namespace SwirlCraft
 
         for (int32_t iter = 0; iter < maxIterations; iter++)
         {
-            for (size_t i = 0; i < N; i++)
-            {
-                p_old[i] = p[i];
-            }
-
-
-            for (size_t i = 0; i < N; i++)
-            {
-                if (collision[i] > 0)
-                {
-                    T A = 0;
-                    for (size_t k = 0; k < Dims; k++)
-                    {
-                        const auto stride = grid.stride[k];
-                        const T b1 = collision[i-stride] > 0 ? p_old[i-stride] : p_old[i];
-                        const T b2 = collision[i+stride] > 0 ? p_old[i+stride] : p_old[i]; 
-                        A += c[k] * (b1 + b2);
-                    }
-                    p[i] = A - c0 * div[i];
-                }
-            }
+            jacobiIteration(p, p_old, div, collision, c0, c, grid.stride, Dims, N);
         }
         
         delete[] p_old;
