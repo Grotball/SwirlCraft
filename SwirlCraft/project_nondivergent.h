3afc7824334ec7ac9be58be091d96f48429054d1
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-12 20:05:51 +0800

    Add projectNonDivergent
    The function, projectNonDivergent, removes divergence from the velocity
    field. This is done by solving for the pressure, and subtracting the
    pressure gradient from velocity.

---
+++ SwirlCraft/project_nondivergent.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "grid.h"
+#include "pressure_solve.h"
+
+namespace SwirlCraft
+{
+    template <typename T, uint32_t Dims>
+    void projectNonDivergent(T* (&vel)[Dims], T* p, T* p_old, T* div, const T* collision, const T* (&collision_vel)[Dims], const Grid<T, Dims>& grid)
+    {
+        for (size_t i = 0; i < grid.N; i++)
+        {
+            if (collision[i] > 0)
+            {
+                div[i] = 0;
+                for (uint32_t j = 0; j < Dims; j++)
+                {
+                    const auto stride = grid.stride[j];
+                    auto b1 = collision[i - stride] > 0 ? vel[j][i - stride] : 2 * collision_vel[j][i - stride] - vel[j][i];
+                    auto b2 = collision[i + stride] > 0 ? vel[j][i + stride] : 2 * collision_vel[j][i + stride] - vel[j][i];
+                    div[i] += (b2 - b1) * static_cast<T>(0.5) / grid.dx[j];
+                }
+            }
+        }
+
+        jacobiSolve(p, p_old, div, collision, grid, 80);
+
+        for (uint32_t i = 0; i < Dims; i++)
+        {
+            auto stride = grid.stride[i];
+            for (size_t j = 0; j < grid.N; j++)
+            {
+                if (collision[j] > 0)
+                {
+                    auto a1 = collision[j-stride] > 0 ? p[j-stride] : p[j];
+                    auto a2 = collision[j+stride] > 0 ? p[j+stride] : p[j];
+
+                    vel[i][j] -= (a2 - a1) / (2 * grid.dx[i]);
+                }
+            }
+        }
+    }
+}
--- SwirlCraft/swirl_craft.h
+++ SwirlCraft/swirl_craft.h
@@ -1,3 +1,4 @@
 #pragma once
 #include "advection.h"
 #include "pressure_solve.h"
+#include "project_nondivergent.h"
