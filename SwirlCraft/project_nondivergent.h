80b2e124610559838e1951b13086360156fbb124
Grotball <140941449+Grotball@users.noreply.github.com>
2024-08-12 21:07:08 +0800

    Add solve method option to projectNonDivergent

--- SwirlCraft/project_nondivergent.h
+++ SwirlCraft/project_nondivergent.h
@@ -5,7 +5,7 @@
 namespace SwirlCraft
 {
     template <typename T, uint32_t Dims>
-    void projectNonDivergent(T* (&vel)[Dims], T* p, T* p_old, T* div, const T* collision, const T* (&collision_vel)[Dims], const Grid<T, Dims>& grid, const int32_t maxIterations=40)
+    void projectNonDivergent(T* (&vel)[Dims], T* p, T* p_old, T* div, const T* collision, const T* (&collision_vel)[Dims], const Grid<T, Dims>& grid, PressureSolveMethod solveMethod=PressureSolveMethod::JacobiMethod, const int32_t maxIterations=40)
     {
         for (size_t i = 0; i < grid.N; i++)
         {
@@ -22,7 +22,19 @@ namespace SwirlCraft
             }
         }
 
-        jacobiSolve(p, p_old, div, collision, grid, maxIterations);
+        switch (solveMethod)
+        {
+            case PressureSolveMethod::GaussSeidelMethod:
+            {
+                gaussSeidelSolve(p, div, collision, grid, maxIterations);
+                break;
+            }
+            default:
+            {
+                jacobiSolve(p, p_old, div, collision, grid, maxIterations);
+                break;
+            }
+        }
 
         for (uint32_t i = 0; i < Dims; i++)
         {
