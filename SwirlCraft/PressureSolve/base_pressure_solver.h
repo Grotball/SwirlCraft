b18bcf1bbc40b99d39c75923774eccef28b20c5a
Grotball <140941449+Grotball@users.noreply.github.com>
2024-09-10 20:02:17 +0800

    Update BasePressureSolver class

--- SwirlCraft/PressureSolve/base_pressure_solver.h
+++ SwirlCraft/PressureSolve/base_pressure_solver.h
@@ -9,11 +9,13 @@ namespace SwirlCraft
     template <template<class, uint32_t> class Derived, typename T, uint32_t Dims>
     class BasePressureSolver
     {
+        int32_t maxIterations;
         protected:
-        BasePressureSolver() = default;
+        Grid<T, Dims> grid;
+        BasePressureSolver(Grid<T, Dims> grid, int32_t maxIterations) : maxIterations(maxIterations), grid(grid) {}
 
         public:
-        void solve(T* p, const T* div, const T* collision, const Grid<T, Dims>& grid, const int32_t maxIterations)
+        void solve(T* p, const T* div, const T* collision, const int32_t maxIterations)
         {
             static_cast<Derived<T, Dims>*>(this)->_solve_impl(p, div, collision, grid, maxIterations);
         }
