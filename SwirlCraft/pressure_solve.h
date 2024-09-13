f4d9c056b7c025f31a0fb1981c94557c9413b917
Grotball <140941449+Grotball@users.noreply.github.com>
2024-09-13 21:58:00 +0800

    Update includes

--- SwirlCraft/pressure_solve.h
+++ SwirlCraft/pressure_solve.h
@@ -3,6 +3,9 @@
 #include "PressureSolve/jacobi.h"
 #include "PressureSolve/gauss_seidel.h"
 #include "PressureSolve/conjugate_gradient.h"
+#include "PressureSolve/jacobi_solver.h"
+#include "PressureSolve/gauss_seidel_solver.h"
+#include "PressureSolve/conjugate_gradient_solver.h" 
 
 
 namespace SwirlCraft
