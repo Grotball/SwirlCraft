82d1436e08bff6703e286c56214c16852639654c
Grotball <140941449+Grotball@users.noreply.github.com>
2024-09-16 21:17:48 +0800

    Add precondition option to CG solver

--- SwirlCraft/PressureSolve/conjugate_gradient_solver.h
+++ SwirlCraft/PressureSolve/conjugate_gradient_solver.h
@@ -10,25 +10,61 @@ namespace SwirlCraft
         private:
         using BasePressureSolver<ConjugateGradientSolver, T, Dims>::grid;
         T epsilon;
+        bool use_preconditioner;
         T* p;
         T* r;
         T* v;
+        // L_diag, w, z used for preconditioned solve
+        T* L_diag;
+        T* w;
+        T* z;
         public:
-        ConjugateGradientSolver(Grid<T, Dims> grid, int32_t maxIterations, T epsilon=0) : BasePressureSolver<ConjugateGradientSolver, T, Dims>(grid, maxIterations), epsilon(epsilon)
+        ConjugateGradientSolver(Grid<T, Dims> grid, int32_t maxIterations, T epsilon=0, bool use_preconditioner=false) : BasePressureSolver<ConjugateGradientSolver, T, Dims>(grid, maxIterations), epsilon(epsilon), use_preconditioner(use_preconditioner)
         {
             p = new  T[grid.N];
             r = new  T[grid.N];
             v = new  T[grid.N];
+            
+            if (use_preconditioner)
+            {
+                L_diag = new T[grid.N];
+                w = new T[grid.N];
+                z = new T[grid.N];
+            }
+            else
+            {
+                
+                L_diag = nullptr;
+                w = nullptr;
+                z = nullptr;
+
+            }
         }
         void _solve_impl(T* f, const T* g, const T* collision, const int32_t maxIterations)
         {
-            conjugateGradientSolve(p, r, v, f, g, collision, grid, maxIterations, epsilon);
+            if (use_preconditioner)
+            {
+                preconditionedConjugateGradientSolve(L_diag, p, r, v, w, z, f, g, collision, grid, maxIterations, epsilon);
+            }
+            else
+            {
+                conjugateGradientSolve(p, r, v, f, g, collision, grid, maxIterations, epsilon);
+
+            }
         }
         ~ConjugateGradientSolver()
         {
             delete[] p;
             delete[] r;
             delete[] v;
+
+            if (use_preconditioner)
+            {
+
+                delete[] L_diag;
+                delete[] w;
+                delete[] z;
+            }
         }
     };
 }
